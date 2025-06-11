3251cc66ecde8b99104c6e57a3b92006d2713845
basant <animeindia.96@gmail.com>
2025-06-11 21:07:19 +0530

    Update config.py

--- helper/config.py
+++ helper/config.py
@@ -19,7 +19,7 @@ try:
     API_HASH = config("API_HASH")
     BOT_TOKEN = config("BOT_TOKEN")
     OWNER = config("OWNER_ID", default=1322549723, cast=int)
-    LOG = config("LOG_CHANNEL", cast=int)
+    LOG = config("LOG_CHANNEL", default=0, cast=int)  # Default to 0 if not set
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
